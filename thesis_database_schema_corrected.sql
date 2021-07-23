68ac4bf4515ae0a17913c9cbc315a0af1908c7ce
Daniil Kozlov <51784302+Kozzlov@users.noreply.github.com>
2021-07-23 20:24:36 +0300

    Update thesis_database_schema_corrected.sql

--- thesis_database_schema_corrected.sql
+++ thesis_database_schema_corrected.sql
@@ -1,5 +1,7 @@
 USE mysql;
 
+USE mysql;
+
 create table education(university_id int primary key,
 					   university_name varchar(50),
 					   university_specialization varchar(20));
@@ -10,6 +12,10 @@ create table application_user(user_id int primary key,
 							  user_name varchar(20),
 							  user_password varchar(20),
                               user_role varchar(20));
+
+ALTER TABLE application_user
+ADD COLUMN user_interests_overview varchar(50) AFTER user_name;
+
                               
 create table connection(user_id int,  
 						conection_id int, 
@@ -80,4 +86,52 @@ create table user_in_project(if_participant bool,
                              user_id int,
                              project_id int,
                              CONSTRAINT cUiP1 foreign key user_in_project(user_id) references application_user(user_id),
-							 CONSTRAINT cUiP2 foreign key user_in_project(project_id) references project(project_id));                 
+							 CONSTRAINT cUiP2 foreign key user_in_project(project_id) references project(project_id));   
+
+insert into education (university_id, university_name, university_specialization) 
+values (1, 'PJATK', 'technical'),
+	   (2, 'MIMUW', 'technical'),
+	   (3, 'Warsaw Univetsity of art', 'art');
+       
+insert into application_user (user_id, first_name, last_name, user_name,
+							  user_password, user_role) 
+values (1, 'emma', 'stone', 'es081898', 'passemma1', 'tech student'),
+	   (2, 'kyle', 'brown', 'kb081999', 'passkyle1', 'art student'),
+       (3, 'anna', 'fry', 'af041998', 'passanna1', 'tech student'),
+       (4, 'tihan', 'kozlov', 'tk011997', 'passtihan1', ' art student'),
+       (5, 'simon', 'black', 'es021996', 'passsimon1', 'tech student');
+
+
+insert into education_user (university_id, user_id)
+values (1, 1),
+	   (1, 3),
+       (2, 5),
+       (3, 2),
+       (3, 4);
+
+insert into skill (skill_id, skill_name, skill_type)
+values (1, 'pytorch', 'tech'),
+	   (2, 'keras', 'tech'),
+       (3, 'tensorflow', 'tech'),
+       (4, 'react', 'tech'),
+       (5, 'flask', 'tech'),
+       (6, 'jango', 'tech'),
+       (7, 'GCP ', 'tech'),
+       (8, 'AWS', 'tech'),
+       (9, 'unity', 'tech'),
+       (10, 'unreal engine', 'tech'),
+       (11, '2D graphics', 'tech'),
+       (12, '3D graphics', 'tech'),
+       (13, 'mathematics', 'strict science'),
+       (14, 'physics', 'strict science'),
+       (15, 'chemistry', 'strict science'),
+       (16, 'biology', 'strict science'),
+       (17, 'mathematics', 'strict science'),
+       (18, 'physics', 'strict science'),
+       (19, 'chemistry', 'strict science'),
+       (20, 'biology', 'strict science');
+       
+insert into skill_user(skill_id, user_id)
+VALUES (1,1),
+       (6,5),
+       (6,3);              
